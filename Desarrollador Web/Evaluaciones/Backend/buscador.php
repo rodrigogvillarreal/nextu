0e3ca137e10abf5d1887c51b95cbfd4ac59e1685
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2017-11-24 02:40:22 -0300

    Archivo de busqueda de datos inmobiliarios

--- Desarrollador Web/Evaluaciones/Backend/buscador.php
+++ Desarrollador Web/Evaluaciones/Backend/buscador.php
@@ -1,3 +1,18 @@
 <?php
 
+$ciudad = htmlspecialchars($_GET['ciudad']);
+$tipo = htmlspecialchars($_GET['tipo']);
+$precio = htmlspecialchars($_GET['precio']);
+
+$pos = strpos($precio, ';');
+$min = substr($precio, 0, $pos);
+$max = substr($precio, $pos+1);
+
+//Lectura del Archivo de datos json
+$file = fopen("data-1.json", "r") or die("No se pudo abrir el archivo");
+$json = fread($file, filesize('data-1.json'));
+$data = json_decode($json, true);
+
+
+
 ?>
