66ffadd1a29bf6f79920bfc4d42f60233b954001
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2017-11-24 02:38:14 -0300

    Archivo de lectura de datos (Test)

--- Desarrollador Web/Evaluaciones/Backend/cargarListas.php
+++ Desarrollador Web/Evaluaciones/Backend/cargarListas.php
@@ -1,3 +1,30 @@
 <?php
 
+//Lectura del Archivo de datos json
+$file = fopen("data-1.json", "r") or die("No se pudo abrir el archivo");
+$json = fread($file, filesize('data-1.json'));
+$data = json_decode($json, true);
+
+//Recupero la lista de Ciudades
+$ciudades = array();
+
+foreach($data as $d){
+  $ciudades[] = $d['Ciudad'];
+}
+
+$ciudades = array_unique($ciudades);
+
+var_dump($ciudades);
+
+//Recupero la lista de Tipos
+$tipos = array();
+
+foreach($data as $d){
+  $tipos[] = $d['Tipo'];
+}
+
+$tipos = array_unique($tipos);
+
+var_dump($tipos);
+
 ?>
