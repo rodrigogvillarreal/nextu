36357528262428d4ba1dd77b628976636bf31b8a
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2017-11-24 23:40:42 -0300

    Configuro la devolucion de las listas

--- Desarrollador Web/Evaluaciones/Backend/js/buscador.js
+++ Desarrollador Web/Evaluaciones/Backend/js/buscador.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
     console.log('Cargar Listas');
     //Cargo las ciudades
-    $.post('../listaCiudades.php', function(data){
-      //$('#selectCiudad').html($('#selectCiudad').html()+data);
-      console.log(data);
+    $.post('listaCiudades.php', function(data){
+      $.each(data, function( key, valor ) {
+        $('#selectCiudad').append($('<option>', {
+            value: valor,
+            text : valor
+        }));
+        $('#selectCiudad').material_select();
+      });
+    }, 'json');
+    //Cargo los tipos
+    $.post('listaTipos.php', function(data){
+      $.each(data, function( key, valor ) {
+        $('#selectTipo').append($('<option>', {
+            value: valor,
+            text : valor
+        }));
+        $('#selectTipo').material_select();
+      });
+    }, 'json');
+
+    $('#mostrarTodos').click(function(){
+      $.post('buscador.php', {todos: 1}, function(data){
+
+      }, 'json');
     });
+
+    $('#formulario').on('submit', function(event){
+      $.post('buscador.php', {todos: '0', ciudad: $('#selectCiudad').val(), tipo: $('#selectTipo').val(), precio: $('#rangoPrecio').val()}, function(data){
+
+      }, 'json');
+      event.preventDefault();
+    });
+
 });
