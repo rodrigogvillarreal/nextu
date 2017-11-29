de94a0d9cac7ef9204a2baabedcdb5d850a259b4
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2017-11-29 02:06:20 -0300

    Realizo las correcciones necesarias segun intento de ntrega 1

--- Desarrollador Web/Evaluaciones/Backend/js/buscador.js
+++ Desarrollador Web/Evaluaciones/Backend/js/buscador.js
@@ -21,27 +21,23 @@ $(document).ready(function() {
       });
     }, 'json');
 
-    //Aca almaceno el contenido base sin propiedades cargadas
-    var contenido_base = $('.colContenido').html();
-    //Aca guardo la funcionalidad del boton mostrar todos para que no se pierda al recargar el bloque html con propiedades
-    //Es una copia de la funcion de abajo mostrarTodos.click(...)
-    var funcion_mostrar_todos = "<script type='text/javascript'>$('#mostrarTodos').click(function(event){$.post('buscador.php', {todos: 1}, function(data){$('.colContenido').html(contenido_base+funcion_mostrar_todos);$.each(data, function( key, valor){$('.colContenido').append(valor);});}, 'json');event.preventDefault();});</script>";
+    $('#mostrarTodos').click(function(){
 
-    $('#mostrarTodos').click(function(event){
+      $('#resultados').html('');
       $.post('buscador.php', {todos: 1}, function(data){
-        $('.colContenido').html(contenido_base+funcion_mostrar_todos);
         $.each(data, function( key, valor){
-          $('.colContenido').append(valor);
+          $('#resultados').append(valor);
         });
       }, 'json');
+
     });
 
     $('#formulario').on('submit', function(event){
 
+      $('#resultados').html('');
       $.post('buscador.php', $("#formulario").serialize(), function(data){
-        $('.colContenido').html(contenido_base+funcion_mostrar_todos);
         $.each(data, function( key, valor){
-          $('.colContenido').append(valor);
+          $('#resultados').append(valor);
         })
       }, 'json');
 
