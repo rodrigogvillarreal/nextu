2a16acb41a218065d93b89df6f1110a6c479a37c
Rodrigo Villarreal <rodrigodj2000@gmail.com>
2018-06-01 18:09:12 -0300

    Agrego el cambio de color (a corregir)

--- Desarrollador Web/Evaluaciones/Frameworks y Librerías Javascript/js/app.js
+++ Desarrollador Web/Evaluaciones/Frameworks y Librerías Javascript/js/app.js
@@ -1,9 +1,22 @@
 
 
 function CambiarColor(){
-
+	var color = $(".main-titulo").css('color');
+	
+	console.log('Cambiando color');	
+	
+	if(color == "rgb(255, 255, 255)"){ //blanco
+		$(".main-titulo").css('color', 'rgb(220, 255, 14)');
+		console.log('Cambiando a color Amarillo');	
+	}
+	
+	if(color == "rgb(220, 255, 14)"){ //amarillo
+		$(".main-titulo").css('color', 'rgb(255, 255, 255)');
+		console.log('Cambiando a color Blanco');	
+	}	
 }
 
 $(document).ready(function(){
 	console.log('Ok');	
+	setInterval(CambiarColor(), 1500);
 });
