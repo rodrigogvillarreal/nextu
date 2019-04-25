23c0ed12d4480f309f14131c127b42bc706e92ad
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2019-04-25 12:14:49 -0300

    5) Agrego el proceso de crear nuevos eventos

--- Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/new_event.php
+++ Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/new_event.php
@@ -1,5 +1,50 @@
 <?php
   
+    session_start();
+    require('./conector.php');
 
+    $conn = new ConectorBD();
 
+    $user = $_SESSION['username'];
+
+    $titulo = $_POST['titulo'];
+    $fechaInicio = $_POST['start_date'];
+    ( $_POST['allDay'] == 'true' ) ? $diaCompleto = 1 : $diaCompleto = 0;
+    $horaInicio = ( $diaCompleto == 0 ) ? $_POST['start_hour'] : NULL;
+    $fechaFinalizacion = ( $diaCompleto == 0 ) ? $_POST['end_date'] : NULL;
+    $horaFinalizacion = ( $diaCompleto == 0 ) ? $_POST['end_hour'] : NULL;
+
+    if( $diaCompleto == 0 ){
+
+        $sql = "insert into eventos(titulo, fechaInicio, horaInicio, fechaFinalizacion, horaFinalizacion, diaCompleto, nombreUsuario) values("
+                . "'" . $titulo . "'," 
+                . "'" . $fechaInicio . "'," 
+                . "'" . $horaInicio . "'," 
+                . "'" . $fechaFinalizacion . "'," 
+                . "'" . $horaFinalizacion . "'," 
+                . " " . $diaCompleto . "," 
+                . "'" . $user . "')";
+
+    } else {
+
+        $sql = "insert into eventos(titulo, fechaInicio, diaCompleto, nombreUsuario) values("
+                . "'" . $titulo . "'," 
+                . "'" . $fechaInicio . "'," 
+                . " " . $diaCompleto . "," 
+                . "'" . $user . "')";
+
+    }
+
+    $resultado = $conn->ejecutarSql($sql);
+
+    if( $resultado == true )
+    {
+        $respuesta['msg'] = 'OK';
+    }else{
+        $respuesta['msg'] = 'Ocurrio un error al guardar el evento!';
+    }
+
+    $conn->cerrarConexion();
+
+    echo json_encode($respuesta);
  ?>
