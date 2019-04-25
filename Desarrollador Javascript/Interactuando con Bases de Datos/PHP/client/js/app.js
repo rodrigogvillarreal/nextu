452c0f1d491926048777fc0f72dcc6f2bbb4537b
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2019-04-25 09:17:36 -0300

    Cambio la fecha por defecto

--- Desarrollador Javascript/Interactuando con Bases de Datos/PHP/client/js/app.js
+++ Desarrollador Javascript/Interactuando con Bases de Datos/PHP/client/js/app.js
@@ -38,7 +38,7 @@ class EventsManager {
         		center: 'title',
         		right: 'month,agendaWeek,basicDay'
         	},
-        	defaultDate: '2016-11-01',
+        	defaultDate: '2019-05-01',
         	navLinks: true,
         	editable: true,
         	eventLimit: true,
