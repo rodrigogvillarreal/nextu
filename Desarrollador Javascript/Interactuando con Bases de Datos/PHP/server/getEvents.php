d7d11f034df0002373bfc62618179ac208b3319c
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2019-04-25 09:16:15 -0300

    4) Agrego el procedimiento que carga los eventos asociados al usuario logueado

--- Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/getEvents.php
+++ Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/getEvents.php
@@ -1,6 +1,34 @@
 <?php
-  
+    session_start();
+    require('./conector.php');
 
+    $conn = new ConectorBD();
 
+    $user = $_SESSION['username'];
+
+    // $respuesta['eventos'][] = array('id' => 1, 'title' => 'Mi evento', 'start' => '2019-04-25');
+    // $respuesta['eventos'][] = array('id' => 2, 'title' => 'Mi otro evento', 'start' => '2019-04-26', 'end' => '2019-04-29');
+    // $respuesta['eventos'][] = array('id' => 3, 'title' => 'Día del Trabajador', 'start' => '2019-05-01');
+
+    //insert into eventos(titulo, fechaInicio, diaCompleto, nombreUsuario) values('Día del Trabajador', '20190501', 1);
+    //select 'Dia del Trabajador', '20190501', 1, nombreUsuario from usuarios
+
+    $sql = "select id, titulo, fechaInicio, horaInicio, fechaFinalizacion, horaFinalizacion, diaCompleto from eventos where nombreUsuario = '".$user."'";
+
+    $resultado = $conn->ejecutarSql($sql);
+
+    $respuesta['msg'] = 'OK';
+
+    while( $fila = $resultado->fetch_array() ){
+        // $respuesta['eventos'][] = array('id' => $fila['id'], 'title' => $fila['titulo'], 'start' => $fila['fechaInicio'], 'end' => '2019-05-02');
+        $respuesta['eventos'][] = array(
+                                        'id' => $fila['id'],
+                                        'title' => $fila['titulo'],
+                                        'start' => $fila['fechaInicio']
+                                    );
+
+    }
+
+    echo json_encode($respuesta);
 
  ?>
