e995279c8d2a46d56bf8b1fddcad4c4250efa88f
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2019-04-25 01:56:21 -0300

    Agrego el archivo con la clase conector que se conecta a la base del proyecto

---
+++ Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/conector.php
@@ -0,0 +1,44 @@
+<?php
+
+    class ConectorBD
+    {
+        protected $host = 'localhost';
+        protected $user = 'developer';
+        protected $pass = 'developer';
+        protected $base = 'bd_php';
+        protected $conn;
+
+        public function __construct(){
+
+            //abro la conexion al instanciar el objecto
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->base);
+
+            if( $this->conn->connect_errno ){
+                echo "Error al conectar a la base de datos: ".$this->conn->connect_errno;
+                return;
+            }
+
+        }
+
+        function ejecutarSql($sql){
+
+            if( $resultado = $this->conn->query($sql) ){
+                return $resultado;
+            }else{
+                return 'Se produjo un error al ejecutar la consulta: '.$this->conn->error;
+            }
+
+        }
+
+        function consultaSql($sql){
+
+            $resultado = $this->conn->query($sql);
+
+        }
+
+        function cerrarConexion(){
+            $this->conn->close();
+        }
+    }
+
+?>
