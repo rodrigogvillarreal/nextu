b92f35ec39390b762103446a3aa4e923818e5a71
Rodrigo Villarreal <rodrigogvillarreal@gmail.com>
2019-04-25 16:48:04 -0300

    6) Agrego el proceso de eliminacion de eventos

--- Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/delete_event.php
+++ Desarrollador Javascript/Interactuando con Bases de Datos/PHP/server/delete_event.php
@@ -1,5 +1,24 @@
 <?php
 
+    require('./conector.php');
 
+    $conn = new ConectorBD();
+
+    $id = $_POST['id'];
+
+    $sql = "delete from eventos where id = ".$id;
+
+    $resultado = $conn->ejecutarSql($sql);
+
+    if( $resultado == true )
+    {
+        $respuesta['msg'] = 'OK';
+    }else{
+        $respuesta['msg'] = 'Ocurrio un error al eliminar el evento!';
+    }
+
+    $conn->cerrarConexion();
+
+    echo json_encode($respuesta);    
 
  ?>
